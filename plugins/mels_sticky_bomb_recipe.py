2b324410c5048f2ca59d1f4007c525efad5b0196
Rikko <rikkolovescats@tutanota.com>
2023-05-16 00:59:58 +0530

    Mel lets you tweak his recipe

--- plugins/mels_sticky_bomb_recipe.py
+++ plugins/mels_sticky_bomb_recipe.py
@@ -8,3 +8,13 @@ class MelsSecret(ba.Plugin):
             "Lo! The veil hath been lifted, uncovering "
             "the covert recipe of the volatile sticky bomb."
         )
+
+    def has_settings_ui(self):
+        return True
+
+    def show_settings_ui(self, source_widget):
+        ba.screenmessage(
+            "Verily, the culinary alchemist, with deft hand "
+            "and cunning mind, doth embark on a daring quest "
+            "to amend the sacred recipe."
+        )
