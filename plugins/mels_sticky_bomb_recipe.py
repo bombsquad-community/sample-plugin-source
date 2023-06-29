1a5d1ded24ec63ec4af527ab3eed4d70675747f5
Rikko <rikkolovescats@tutanota.com>
2023-06-30 01:31:57 +0530

    Update recipe to API 8

--- plugins/mels_sticky_bomb_recipe.py
+++ plugins/mels_sticky_bomb_recipe.py
@@ -1,10 +1,10 @@
-# ba_meta require api 7
-import ba
+# ba_meta require api 8
+import babase
 
-# ba_meta export plugin
-class MelsSecret(ba.Plugin):
+# ba_meta export babase.Plugin
+class MelsSecret(babase.Plugin):
     def on_app_running(self):
-        ba.screenmessage(
+        babase.screenmessage(
             "Lo! The veil hath been lifted, uncovering "
             "the covert recipe of the volatile sticky bomb."
         )
@@ -13,7 +13,7 @@ class MelsSecret(ba.Plugin):
         return True
 
     def show_settings_ui(self, source_widget):
-        ba.screenmessage(
+        babase.screenmessage(
             "Verily, the culinary alchemist, with deft hand "
             "and cunning mind, doth embark on a daring quest "
             "to amend the sacred recipe."
